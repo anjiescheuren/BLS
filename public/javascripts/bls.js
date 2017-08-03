705a3880b0ba64be56ee50d4d973b87df65de4a9
Anjie Scheuren <emailanjie@gmail.com>
2017-08-03 09:15:24 -0700

    no data but no errors

--- public/javascripts/bls.js
+++ public/javascripts/bls.js
@@ -1,11 +1,19 @@
 $(function() {
   $.ajax({
       type: 'GET',
-      url: 'https://api.bls.gov/publicAPI/v2/timeseries/data/APU0000702421',
+      headers: {"http://localhost:3000": "localhost"},
+      url: 'https://api.bls.gov/publicAPI/v1/timeseries/data/APU0000702421',
       data: { },
       dataType: 'json'
     })
       .done(function(data) {
         console.log(data);
       })
-    });
+  // $.ajax({
+  //   type: "GET",
+  //   headers: {"http://localhost:3000/": "localhost"},
+  //   url: "https://api.bls.gov/publicAPI/v1/timeseries/data/APU0000702421"
+  // }).done(function (data) {
+  //   console.log(data);
+  //   });
+});
